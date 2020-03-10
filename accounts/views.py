42cb7411244a5df5320315a8a34cef0a67984dd6
johnj974 <jjdug@hotmail.com>
2020-03-10 15:13:43 +0000

    initial commit

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "You have been successfully logged out")
+    return redirect(reverse('index'))

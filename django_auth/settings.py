5ccb032ddb845943843c7989adfaaa561d2b0a8b
johnj974 <jjdug@hotmail.com>
2020-03-11 17:15:18 +0000

    static files connected

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'accounts',
+    'django_forms_bootstrap'
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
